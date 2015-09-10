749ebfcd58ee0a1854c23dca7864270b0ebbb625
Vuorivirta Hanna M A <hvuorivi@da3-cs-dk110-12.helsinki.fi>
2015-09-10 13:17:32 +0300

    kaikkee

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,10 +1,10 @@
--- Kayttaja-taulun testidata
+/*-- Kayttaja-taulun testidata
 INSERT INTO Kayttaja (name, password) VALUES ('Hanna', 'Hanna123');
 INSERT INTO Kayttaja (name, password) VALUES ('Katja', 'Katja123');
 -- Laji-taulun testidata
 INSERT INTO Laji (name) VALUES ('jooga');
 INSERT INTO Laji (name) VALUES ('TRE');
-INSERT INTO Laji (name) VALUES ('taidetuokio');
+INSERT INTO Laji (name) VALUES ('taidetuokio');*/
 /*-- Tunti-taulun testidata, tämän pitää vielä viitata Lajiin...
 INSERT INTO Tunti (kesto) VALUES ('90');
 INSERT INTO Tunti (kesto) VALUES ('90');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,22 +2,22 @@
 
 CREATE TABLE Kayttaja(
     id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
-    name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
-    password varchar(50) NOT NULL
+    --name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+    --password varchar(50) NOT NULL
 );
 
 -- 2 erilaista Kayttajaa: yllapitaja, asiakas, miten?
 
 CREATE TABLE Laji(
-    id SERIAL PRIMARY KEY
-    name varchar(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    --name varchar(50) NOT NULL,
     --description varchar(400) ehkä
 );
 
 -- jooga, TRE, meditaatio, pranayama, taidetuokio
 
 CREATE TABLE Tunti(
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     --/*player_id INTEGER*/ REFERENCES Laji(name)
     --kesto INT NOT NULL
 );
@@ -25,7 +25,7 @@ CREATE TABLE Tunti(
 --  pvm?, klo?, kesto(min), x/x?; jos on täynnä
 
 CREATE TABLE Varaus(
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
 );
 
 -- mitä tänne tulee? Kayttaja id, Tunti id?
